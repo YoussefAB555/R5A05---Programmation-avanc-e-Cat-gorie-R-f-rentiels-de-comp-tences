e9665a2c117731acbf45dbc050628403b108055e
Youss <yidpro555@gmail.com>
2025-11-07 02:15:13 +0100

    Bugfix: Importation de AuthenticationForm dans applicompte/views.py

--- webtravel/applicompte/views.py
+++ webtravel/applicompte/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from .models import TravelUser
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
 from .forms import TravelUserForm, TravelUserImageForm
 
